24b54e4fd261ddc4430696683edabee7ada1efd1
Raunak <raunakparmar1@gmail.com>
2024-10-02 10:35:11 +0530

    added

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,15 +1,25 @@
-export const CartReducer = (state,action) => {
-    const {type,payload} = action;
-
-    switch(type){
+// src/reducers/CartReducer.js
+export const CartReducer = (state, action) => {
+    switch(action.type) {
         case "ADD_TO_CART":
-            return {...state,cartList:payload.products, total:payload.total}
+            return {
+                ...state,
+                cartList: action.payload.products,
+                total: action.payload.total
+            };
         case "REMOVE_FROM_CART":
-            return {...state,cartList:payload.products,total:payload.total}
+            return {
+                ...state,
+                cartList: action.payload.products,
+                total: action.payload.total
+            };
         case "CLEAR_CART":
-            return {...state,cartList:payload.products,total:payload.total}
-        
-        default: 
-            throw new Error("NO case found!");
+            return {
+                ...state,
+                cartList: [],
+                total: 0
+            };
+        default:
+            return state;
     }
-}
+};
