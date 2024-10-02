5eaca4bb1d0b49b3e02b92b7223e840808bcf1ae
Raunak <raunakparmar1@gmail.com>
2024-10-02 10:43:53 +0530

    added

--- src/context/CartContexts.js
+++ src/context/CartContexts.js
@@ -1,6 +1,5 @@
 import { createContext, useContext, useReducer } from "react";
-import { CartReducer } from "../reducers/CartReducer";
-
+import { CartReducer } from "../reducers";
 const cartInitialState = {
     cartList : [],
     total:0
--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -23,3 +23,4 @@ export const CartReducer = (state, action) => {
             return state;
     }
 };
+
